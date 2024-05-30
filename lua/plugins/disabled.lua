ae5c290f16abc5e895ab74e875bf3cb5425abc2d
Yuxuan Chen <yuxuanchen1997@outlook.com>
2024-05-30 13:16:36 -0700

    Create disabled.lua

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,8 @@
+return {
+  { "rcarriga/nvim-notify", enabled = false },
+  { "williamboman/mason.nvim", enabled = false },
+  { "williamboman/mason-lspconfig.nvim", enabled = false },
+  { "nvim-lspconfig", enabled = false },
+--  { "nvim-treesitter", enabled = false },
+--  { "nvim-treesitter-textobjects", enabled = false },
+}
